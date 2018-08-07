b1e51b1ade060cfdd2d2bc051d66423f3f22cb7d
Nivans <regs@nivans.ru>
2018-08-07 15:14:03 +0300

    progress..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
+import { PagePortfolioComponent } from './pages/page-portfolio/page-portfolio.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PageHomeComponent
+  },
+  {
+    path: 'portfolio',
+    component: PagePortfolioComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHomeComponent,
+    PagePortfolioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
