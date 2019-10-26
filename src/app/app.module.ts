7b5a680c57fa5c9a15783b0446bbca0842318bb1
nivans <to@nivans.pro>
2019-10-26 22:07:53 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule, Routes } from '@angular/router';
-import { PageHomeComponent } from './pages/page-home/page-home.component';
-import { PagePortfolioComponent } from './pages/page-portfolio/page-portfolio.component';
-import { HttpClientModule } from '@angular/common/http';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: PageHomeComponent
-  },
-  {
-    path: 'portfolio',
-    component: PagePortfolioComponent
-  }
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PageHomeComponent,
-    PagePortfolioComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    RouterModule.forRoot(routes)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
